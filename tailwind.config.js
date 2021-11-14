03b3d52bb062253958846811c6ea027c471ca4c6
tashenoff <kail.dead.kd@gmai.com>
2021-11-15 00:46:44 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'jit',
+  purge: ["./src/**/*.html", "./src/**.{js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
